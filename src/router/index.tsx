401721091f0e4431426e3d321ac350e5017595e9
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-07-26 23:13:39 -0300

    feat(creating router component and defining home and history routes): :sparkles:

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,13 +1,18 @@
+import { History } from '@pages/History'
+import { Home } from '@pages/Home'
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import { Home } from '../pages/Home'
+import { DefaultLayout } from '../layouts/DefaultLayout'
 
 export function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<DefaultLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/historico" element={<History />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
