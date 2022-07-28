651e0ea43e71acef0cffdb0b9f8353f91302a0f8
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-07-28 12:33:15 -0300

    feat(Adding title to navlinks and updating hover and focus styles :art:): :art:

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,18 @@
 import timerLogo from '@assets/img/logo.png'
 import { Scroll, Timer } from 'phosphor-react'
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 import * as C from './styles'
 
 export function Header() {
   return (
     <C.Header>
-      <Link to="/">
-        <C.Logo src={timerLogo} alt="" />
-      </Link>
+      <C.Logo src={timerLogo} alt="" />
       <C.NavLinks>
-        <C.NavLink to="/">
+        <C.NavLink title="Timer" to="/">
           <Timer size="22" />
         </C.NavLink>
-        <C.NavLink to="/historico">
+        <C.NavLink title="Histórico" to="/historico">
           <Scroll size="22" />
         </C.NavLink>
       </C.NavLinks>
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -6,7 +6,6 @@ export const Header = styled.header`
   width: 100%;
   align-items: center;
   justify-content: space-between;
-  padding: 2.5rem;
 `
 
 export const Logo = styled.img`
@@ -23,9 +22,16 @@ export const NavLinks = styled.nav`
 export const NavLink = styled(NavLinkBase)`
   color: ${({ theme }) => theme.colors.title};
   padding: 0.75rem;
+  border-bottom: 3px solid transparent;
+  border-top: 3px solid transparent;
+  transition: border 0.2s;
+
+  &:hover {
+    border-bottom: 3px solid ${({ theme }) => theme.colors.primary};
+  }
 
   &:focus {
-    border-radius: 0.25rem;
+    border-radius: 4px;
   }
 
   &.active {
